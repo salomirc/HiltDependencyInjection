458429fb4f2f871463a1a958bc055f7a89c4f768
Ciprian Salomir <ciprian.salomir@gmail.com>
2023-03-24 12:37:11 +0200

    Field & Constructor Injection

--- app/src/main/java/com/example/hiltdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/example/hiltdependencyinjection/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.hiltdependencyinjection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, someClass.doAThing())
+        Log.d(TAG, someClass.doSomeOtherThing())
     }
+}
+
+
+// constructor injection
+class SomeClass @Inject constructor(
+    private val someOtherClass: SomeOtherClass
+) {
+
+    fun doAThing(): String = "Look I did a thing!"
+    fun doSomeOtherThing(): String = someOtherClass.doSomeOtherThing()
+}
+
+class SomeOtherClass @Inject constructor() {
+    fun doSomeOtherThing(): String = "Look I did some other thing!"
 }
